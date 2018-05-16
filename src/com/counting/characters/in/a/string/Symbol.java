7d2a5c430741faa212ba26db963ffd76a74d20a2
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-05-16 12:17:52 +0300

    my count

---
+++ src/com/counting/characters/in/a/string/Symbol.java
@@ -0,0 +1,11 @@
+package com.counting.characters.in.a.string;
+import java.io.*;
+import java.util.Arrays;
+
+public class Symbol {
+		String string = "Hello World!";
+		char[] stringToArray = string.toCharArray();
+		for (int i = 0; i < stringToArray.length; i++) {	
+		}
+}
+
