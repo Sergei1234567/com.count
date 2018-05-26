fa73f22e5ae9661a94ebd3b1cce7fcb6ad33e9eb
Sergei Berezhnoi <berezhnoi.serg@gmail.com>
2018-05-26 18:14:46 +0300

    my count

--- src/com/counting/characters/in/a/string/SymbolCount.java
+++ src/com/counting/characters/in/a/string/SymbolCount.java
@@ -1,17 +1,35 @@
 package com.counting.characters.in.a.string;
 
-import java.io.*;
-import java.util.Arrays;
-//java.lang.Character;
-
 class SymbolCount {
 
 	public String toSentence(String sentence) {
 		char[] characters = sentence.toCharArray();
-		sentence = "";
-		for (char calc : characters) {
-				sentence += calc;
+		String result = "";
+		char[] uniqueCharacters = new char[characters.length];
+		for(int i = 0; i < characters.length; i++) {
+			int counter = 0;
+			char c = characters[i];
+			for(char tempTi : characters) {
+				if(tempTi == c) {
+					counter++;
+				}
+			}
+			
+			if(counter == 1) {
+				uniqueCharacters[i] = c;
+			}
 		}
-		return sentence;
+		
+		for (char calc :uniqueCharacters) {
+			int counter = 0;
+			for(char tempCalc : characters) {
+				if(tempCalc == calc) {
+					counter++;
+				}
+			}
+			System.out.println(calc + " -> " + counter);
+		}
+		return result;
+		
 	}
 }
--- src/com/counting/characters/in/a/string/TestSymbolCount.java
+++ src/com/counting/characters/in/a/string/TestSymbolCount.java
@@ -2,8 +2,8 @@ package com.counting.characters.in.a.string;
 
 public class TestSymbolCount {
 	public static void main(String[] args) {
-	SymbolCount count = new SymbolCount();
-	String sentence = count.toSentence("Hello World!");
-	System.out.println(sentence);
+		SymbolCount count = new SymbolCount();
+		String sentence = count.toSentence("Hello World!");
+		System.out.println(sentence);
 	}
 }
